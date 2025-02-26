de065c103540e09a871cc406d350c631b07990c9
yxzhin <yxzhin@gmail.com>
2025-02-26 16:33:50 +0100

    feat!: server init,  main page added, second page added

---
+++ src/app.py
@@ -0,0 +1,29 @@
+
+
+def main():
+
+    from flask import Flask, render_template
+
+    import conf.server
+    import conf.website
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    @app.route("/index.html")
+    def index():
+        return render_template(fr"index.html", title=conf.website.TITLE)
+
+    @app.route("/drugaStranica.html")
+    def drugaStranica():
+        return render_template(fr"drugaStranica.html", title=conf.website.TITLE)
+
+    app.run(
+        host=conf.server.HOST,
+        port=7373,
+        debug=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
